bf51ea45904bb1e1fdf04ef47d3e0002878fec26
Zahid <zahidin98@gmail.com>
2019-11-20 11:25:05 +0700

    add connection pool in sequelize

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,8 +2,27 @@ import { Sequelize } from "sequelize";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+const connectionPool: any =
+  process.env.APP_ENV == "developemnt"
+    ? {}
+    : {
+        pool: {
+          max: 5,
+          min: 0,
+          acquire: 30000,
+          idle: 10000
+        }
+      };
+
 const connect = new Sequelize(
-  `${process.env.DB_DRIVER}://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+  `${process.env.DB_DRIVER}://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  {
+    ...connectionPool,
+    logging:
+      process.env.DB_LOGGING != "false"
+        ? (...msg: any) => console.log(msg)
+        : false
+  }
 );
 
 export default connect;
