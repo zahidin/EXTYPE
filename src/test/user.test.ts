ede05273a82fcb447041a2a61f69d8e99ce081ca
Zahid <zahidin98@gmail.com>
2019-11-20 23:07:47 +0700

    add prefix url in test

--- src/test/user.test.ts
+++ src/test/user.test.ts
@@ -1,13 +1,14 @@
 import request from "supertest";
 import router from "./initialize-express";
 import promiseRequest from "request-promise";
+import { prefixApiV1 } from "../config/prefix-url";
 
 jest.mock("request-promise");
 (promiseRequest as any).mockImplementation(() => '{"features": []}');
 
 describe("user", () => {
   test("a test data user", async () => {
-    const response = await request(router).get("/api/v1/users");
+    const response = await request(router).get(`${prefixApiV1}/users`);
     expect(response.status).toEqual(200);
   });
 });
